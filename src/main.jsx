46aedd5f2905fd635a79509267b1db148aae081b
Atilla.G <atillagoguslu@hotmail.com>
2025-05-24 14:15:46 +0300

    Update project configuration: change repository URL, simplify build command, and remove basename from BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/slim-mom-frontend">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
